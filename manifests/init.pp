0a8878e9fe82edefa529e79d0667e27c5475395e
Andrew Leonard <andy@hurricane-ridge.com>
2013-05-26 06:46:01 -0700

    Add comments to init.pp; otherwise, do nothing in the parent class.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,41 +1,23 @@
 # == Class: firewallrules
 #
-# Full description of class firewallrules here.
+# Default pre and post firewall rules, for use with puppetlabs/firewall.
 #
-# === Parameters
-#
-# Document parameters here.
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if it
-#   has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should not be used in preference to class parameters  as of
-#   Puppet 2.6.)
+# The main class is a no-op.
 #
 # === Examples
 #
-#  class { firewallrules:
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ]
-#  }
+#  include firewallrules
 #
 # === Authors
 #
-# Author Name <author@domain.com>
+# Andrew Leonard
 #
 # === Copyright
 #
-# Copyright 2013 Your name here, unless otherwise noted.
+# Copyright 2013 Andrew Leonard
 #
 class firewallrules {
 
+  # Do nothing
 
 }
