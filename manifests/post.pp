aa093cd9943dd9aafb02b4cd4ce5831a4fb5a9a4
Andrew Leonard <andy@hurricane-ridge.com>
2013-07-03 19:56:26 -0700

    Add default v6 drop all rule.

--- manifests/post.pp
+++ manifests/post.pp
@@ -26,4 +26,13 @@ class firewallrules::post {
     action  => 'drop',
     before  => undef,
   }
+
+  unless $::ipaddress6 == undef {
+    firewall { '999 drop v6 all':
+      proto    => 'all',
+      action   => 'drop',
+      before   => undef,
+      provider => 'ip6tables'
+    }
+  }
 }
